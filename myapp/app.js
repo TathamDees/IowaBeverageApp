1b09ff68b3edb4929b735f077ac604c0a2ee279b
Vedant <vedant.gandhewar@gmail.com>
2019-04-14 15:27:02 -0500

    Connected to MySql database

--- myapp/app.js
+++ myapp/app.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'iowa_alcohol'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to database...");
+});
+
+
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
