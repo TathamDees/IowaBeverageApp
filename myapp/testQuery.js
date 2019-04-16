87495271438aa42b374bf579209179c3703db097
Vedant <vedant.gandhewar@gmail.com>
2019-04-15 20:54:31 -0500

    Minor fix with concatenation in queries

--- myapp/testQuery.js
+++ myapp/testQuery.js
@@ -2,8 +2,8 @@ module.exports = {
 
     //Lists out the top x alcohols by the number of bottles sold
     topxAlcohol: (req, res, x) => {
-        let theQuery = "SELECT item_desc FROM iowa_liquor_sales GROUP BY item_desc ORDER BY SUM(bottle_sold) DESC LIMIT " + x;
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT item_desc FROM iowa_liquor_sales GROUP BY item_desc ORDER BY SUM(bottle_sold) DESC LIMIT " + x;
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -12,20 +12,18 @@ module.exports = {
     
     //number of unique beverages
     uniqueBeverageCount: (req, res) => {
-        let theQuery = "SELECT count(DISTINCT item_desc) FROM iowa_liquor_sales; ";
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT count(DISTINCT item_desc) FROM iowa_liquor_sales; ";
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
         });
     },
 
-    //all beverages that start with the input character                 (DOESN'T WORK CURRENTLY)
+    //all beverages that start with the input character
     beverageStartingWith: (req, res, startChar) => {
-        let theQuery = "SELECT DISTINCT item_desc FROM iowa_liquor_sales WHERE LEFT(item_desc , 1) = 'A' ORDER BY item_desc;";
-        //let theQuery = "SELECT DISTINCT item_desc FROM iowa_liquor_sales WHERE LEFT(item_desc , 1) = ' + startChar + ' ORDER BY item_desc;";
-        //let theQuery = 'SELECT DISTINCT item_desc FROM iowa_liquor_sales WHERE LEFT(item_desc , 1) = ${startChar} ORDER BY item_desc;';
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT DISTINCT item_desc FROM iowa_liquor_sales WHERE LEFT(item_desc , 1) = '" + startChar + "' ORDER BY item_desc;";
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -34,8 +32,8 @@ module.exports = {
     
     //beverages sold by x store
     soldByStore: (req, res) => {
-        let theQuery = "SELECT DISTINCT item_desc FROM iowa_liquor_sales WHERE store_name = 'Central City 2' ORDER BY item_desc";
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT DISTINCT item_desc FROM iowa_liquor_sales WHERE store_name = 'Central City 2' ORDER BY item_desc";
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -44,8 +42,8 @@ module.exports = {
 
     //top x alcohols by city
     topAlcoholByCity: (req, res, x) => {
-        let theQuery = "SELECT DISTINCT item_desc , city , SUM(bottle_sold) FROM iowa_liquor_sales WHERE city = 'DES MOINES' GROUP BY item_desc ORDER BY SUM(bottle_sold) DESC LIMIT " + x;
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT DISTINCT item_desc , city , SUM(bottle_sold) FROM iowa_liquor_sales WHERE city = 'DES MOINES' GROUP BY item_desc ORDER BY SUM(bottle_sold) DESC LIMIT " + x;
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -54,8 +52,8 @@ module.exports = {
 
     //displays a list of stores that sell x alcohol
     storesThatSell: (req, res) => {
-        let theQuery = "SELECT DISTINCT store_name FROM iowa_liquor_sales WHERE item_desc = 'Godiva Liqueur' ORDER BY store_name;";
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT DISTINCT store_name FROM iowa_liquor_sales WHERE item_desc = 'Godiva Liqueur' ORDER BY store_name;";
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -64,8 +62,8 @@ module.exports = {
 
     //Displays the top x most ordered beverages by ___ store
     popularByStore: (req, res, x) => {
-        let theQuery = "SELECT item_desc , store_name , SUM(bottle_sold) AS 'Total Bottles Sold' FROM iowa_liquor_sales WHERE store_name = 'Central City 2' GROUP BY item_desc ORDER BY SUM(bottle_sold) DESC LIMIT " + x;
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT item_desc , store_name , SUM(bottle_sold) AS 'Total Bottles Sold' FROM iowa_liquor_sales WHERE store_name = 'Central City 2' GROUP BY item_desc ORDER BY SUM(bottle_sold) DESC LIMIT " + x;
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -74,8 +72,8 @@ module.exports = {
 
     //Displays the top x stores that sold the most bottles
     mostPopularStores: (req, res, x) => {
-        let theQuery = "SELECT store_name , SUM(bottle_sold) FROM iowa_liquor_sales GROUP BY store_name ORDER BY Count(item_desc) DESC LIMIT " + x;
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT store_name , SUM(bottle_sold) FROM iowa_liquor_sales GROUP BY store_name ORDER BY Count(item_desc) DESC LIMIT " + x;
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
@@ -84,19 +82,21 @@ module.exports = {
 
     //Displays the top x stores with the highest revenue
     storeByRevenue: (req, res, x) => {
-        let theQuery = "SELECT store_name , SUM(sale) FROM iowa_liquor_sales GROUP BY store_name ORDER BY Count(item_desc) DESC LIMIT " + x;
-        db.query(theQuery , function (err, results) {
+        let query = "SELECT store_name , SUM(sale) FROM iowa_liquor_sales GROUP BY store_name ORDER BY Count(item_desc) DESC LIMIT " + x;
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
         });
     },
+
+    
     
     /*
     //
     method: (req, res) => {
-        let theQuery = "";
-        db.query(theQuery , function (err, results) {
+        let query = "";
+        db.query(query , function (err, results) {
             if (err)
                 throw err;
             res.send(JSON.stringify(results));
