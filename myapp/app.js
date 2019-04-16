abd5f4d9bc8dcc21b556c967817579e4733321a9
Martin <martin.wang@vanderbilt.edu>
2019-04-16 13:20:02 -0500

    Martin algorithm commit

--- myapp/app.js
+++ myapp/app.js
@@ -17,27 +17,6 @@ global.db = db;
 
 app.use(express.static('C:/Users/Martin Wang/iowaAlc/myapp/frontEnd'));
 
-app.get('/', function (req, res) {
-  //test.mostPopularStoresByYear(req, res, 2018, 5);
-  //test.storeByRevenueByCity(req, res, 'Waterloo', 5);
-  //test.topxAlcohol(req, res, 3);
-  //test.uniqueBeverageCount(req, res);
-  //test.beverageStartingWith(req, res, 'a');
-  ///test.topxAlcohol(req, res, 3);
-  ///test.uniqueBeverageCount(req, res);
-  ///test.beverageStartingWith(req, res, 'a');
-  //test.soldByStore(req , res, 'Central City 2');
-  //test.topAlcoholByCity(req , res , 'DES MOINES', 10);
-  test.storesThatSell(req, res, 'Godiva Liqueur');
-  //test.popularByStore(req , res , 'Central City 2', 5);
-  //test.mostPopularStores(req , res , 5);
-  //test.storeByRevenue(req , res , 5);
-  //test.efficientDrinks(req , res , 10);
-  //test.expensiveDrinks(req , res , 10);
-  //test.liqourStoresInCity(req , res , 'DES MOINES');
-  //test.randomBeverageSelector(req , res , 3);
-  //test.cocktailGenerator(req , res , 3 , 4);
-});
 
 
 app.get('/', function (req, res) {
@@ -48,33 +27,33 @@ app.get('/', function (req, res) {
   // })
 });
 
-app.get("/foo", function (req, res) {
-  console.log("ksjdgkjnfdskjgd");
-  //test.mostPopularStoresByYear(req, res, 2018, 5);
-  //test.storeByRevenueByCity(req, res, 'Waterloo', 5);
-  //test.topxAlcohol(req, res, 3);
-  //test.uniqueBeverageCount(req, res);
-  //test.beverageStartingWith(req, res, 'a');
-  test.topxAlcohol(req, res, 3);
-  ///test.uniqueBeverageCount(req, res);
-  ///test.beverageStartingWith(req, res, 'a');
-  //test.soldByStore(req , res, 'Central City 2');
-  //test.topAlcoholByCity(req , res , 'DES MOINES', 10);
-  //test.storesThatSell(req , res, 'Godiva Liqueur');
-  //test.popularByStore(req , res , 'Central City 2', 5);
-  //test.mostPopularStores(req , res , 5);
-  //test.storeByRevenue(req , res , 5);
-  //test.efficientDrinks(req , res , 10);
-  //test.expensiveDrinks(req , res , 10);
-  //test.liqourStoresInCity(req , res , 'DES MOINES');
-  //test.randomBeverageSelector(req , res , 3);
-  //test.cocktailGenerator(req, res, 3, 4);
-});
+// app.get("/foo", function (req, res) {
+//   console.log("ksjdgkjnfdskjgd");
+//   //test.mostPopularStoresByYear(req, res, 2018, 5);
+//   //test.storeByRevenueByCity(req, res, 'Waterloo', 5);
+//   //test.topxAlcohol(req, res, 3);
+//   //test.uniqueBeverageCount(req, res);
+//   //test.beverageStartingWith(req, res, 'a');
+//   //test.topxAlcohol(req, res, 3);
+//   ///test.uniqueBeverageCount(req, res);
+//   ///test.beverageStartingWith(req, res, 'a');
+//   //test.soldByStore(req , res, 'Central City 2');
+//   //test.topAlcoholByCity(req , res , 'DES MOINES', 10);
+//   //test.storesThatSell(req , res, 'Godiva Liqueur');
+//   //test.popularByStore(req , res , 'Central City 2', 5);
+//   //test.mostPopularStores(req , res , 5);
+//   //test.storeByRevenue(req , res , 5);
+//   //test.efficientDrinks(req , res , 10);
+//   //test.expensiveDrinks(req , res , 10);
+//   //test.liqourStoresInCity(req , res , 'DES MOINES');
+//   //test.randomBeverageSelector(req , res , 3);
+//   //test.cocktailGenerator(req, res, 3, 4);
+// });
 app.get("/cocktail", function (req, res) {
   test.cocktailGenerator(req, res, 3, 4);
 });
 app.get("/topAlcohol", function (req, res) {
-  test.topxAlcohol(req, res, 3);
+  test.topxAlcohol(req, res, req.query.param);
 });
 
 app.listen(8080, function () {
